5b3e43cd3cb56dafe108519eadba7af9f4bb01f5
Rommel Rico <me@rommelrico.com>
2018-12-29 13:17:47 -0800

    Adding onChange callback

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,13 +4,21 @@ class SearchBar extends React.Component {
 
     state = { searchTerm: '' };
 
+    onInputChange = () => {
+        // Implement me;
+    };
+
     render() {
         return (
             <div className="search-bar ui segment">
                 <form className="ui form">
                     <div className="field">
                         <label>Video Search:</label>
-                        <input type="text" value={this.state.searchTerm} />
+                        <input
+                            type="text"
+                            value={this.state.searchTerm}
+                            onChange={this.onInputChange}
+                        />
                     </div>
                 </form>
             </div>
