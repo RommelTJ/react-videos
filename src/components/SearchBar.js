a95f12c63e394a132135b93f03cd3d02a66876c2
Rommel Rico <me@rommelrico.com>
2018-12-29 13:11:28 -0800

    Adding ui segment class to Searchbar div

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,7 +4,9 @@ class SearchBar extends React.Component {
 
     render() {
         return (
-            <div>Search Bar</div>
+            <div className="search-bar ui segment">
+
+            </div>
         );
     }
 
