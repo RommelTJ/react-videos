61770e07303bbe808ab001c38b6b3dfab49dace8
Rommel Rico <me@rommelrico.com>
2018-12-29 13:12:38 -0800

    Video Search label

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,7 +7,7 @@ class SearchBar extends React.Component {
             <div className="search-bar ui segment">
                 <form className="ui form">
                     <div className="field">
-                        
+                        <label>Video Search:</label>
                     </div>
                 </form>
             </div>
