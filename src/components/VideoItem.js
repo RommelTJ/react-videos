46a8b4fd923ed09064c261d2aa6b6643b2e3c198
Rommel Rico <me@rommelrico.com>
2018-12-30 15:37:18 -0800

    Displaying video thumbnails

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -4,6 +4,7 @@ const VideoItem = ({ video }) => {
 
     return (
         <div>
+            <img src={video.snippet.thumbnails.medium.url} />
             {video.snippet.title}
         </div>
     );
